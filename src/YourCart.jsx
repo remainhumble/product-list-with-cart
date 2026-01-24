de075bef284ca5e5bd7e46643befdfbdf4133082
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2026-01-24 19:27:30 +1300

    total calculation

--- src/YourCart.jsx
+++ src/YourCart.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './App.css'
 
+/**
+ * YourCart component displays the items in the user's shopping cart.
+ * 
+ * @param {Object} props - The component props.
+ * @param {Array} props.cartItems - An array of items currently in the cart, where each item is an object containing:
+ *   @param {string} item.name - The name of the item.
+ *   @param {number} item.price - The price of the item.
+ *   @param {number} item.quantity - The quantity of the item in the cart.
+ * @param {Function} props.onRemoveFromCart - A function to handle removing an item from the cart, which takes the item's name as an argument.
+ * 
+ * The total number of items in the cart is calculated by summing the quantity of each item in the cart.
+ * The order total is calculated by multiplying the price of each item by its quantity and summing these values for all items in the cart.
+ */
 const YourCart = ({ cartItems, onRemoveFromCart }) => {
     const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0)
 
@@ -28,9 +41,9 @@ const YourCart = ({ cartItems, onRemoveFromCart }) => {
                                             <p>${(item.quantity * item.price).toFixed(2)}</p>
                                         </div>
                                         <div className='remove'>
-                                            <img 
-                                                className='remove-icon' 
-                                                src="assets/images/icon-remove-item.svg" 
+                                            <img
+                                                className='remove-icon'
+                                                src="assets/images/icon-remove-item.svg"
                                                 alt="remove-item"
                                                 onClick={() => onRemoveFromCart(item.name)}
                                                 style={{ cursor: 'pointer' }}
@@ -41,6 +54,19 @@ const YourCart = ({ cartItems, onRemoveFromCart }) => {
                             ))}
                         </div>
                     )}
+                </div>
+                <div className="order-total">
+                    {// Checks if there are any items in the cart
+                        cartItems.length > 0 && (
+                            <>
+                                <span>Order Total:</span>
+                                <span className="total">
+                                    ${cartItems
+                                        .reduce((total, item) => total + item.quantity * item.price, 0)
+                                        .toFixed(2)}
+                                </span>
+                            </>
+                        )}
                 </div>
             </div>
         </>
